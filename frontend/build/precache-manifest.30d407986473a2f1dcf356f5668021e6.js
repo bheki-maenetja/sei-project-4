944c437fabcd015cbb80741a9c6e28b25a3dbd69
bheki-maenetja <bhekimaenetja@gmail.com>
2020-03-05 13:35:20 +0000

    deploy test

---
+++ frontend/build/precache-manifest.30d407986473a2f1dcf356f5668021e6.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "898ea3d6d36f19ae51af1db7873f704c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "55b4a7ea1b6580c57295",
+    "url": "/static/css/2.3d0875a0.chunk.css"
+  },
+  {
+    "revision": "2c0cffad57558f9db738",
+    "url": "/static/css/main.3ab325f0.chunk.css"
+  },
+  {
+    "revision": "55b4a7ea1b6580c57295",
+    "url": "/static/js/2.1877f56c.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/static/js/2.1877f56c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c0cffad57558f9db738",
+    "url": "/static/js/main.c52d9ea9.chunk.js"
+  },
+  {
+    "revision": "a8139511e7e3c6f8467f",
+    "url": "/static/js/runtime-main.1c2d59f4.js"
+  },
+  {
+    "revision": "fccc69b9da186b315ebc3737d99c763b",
+    "url": "/static/media/auth-page-background.fccc69b9.jpg"
+  },
+  {
+    "revision": "4af6c00c0a7615861b524164ac695abd",
+    "url": "/static/media/hero-battle-carousel.4af6c00c.jpeg"
+  },
+  {
+    "revision": "90807fe2a8542ac5d06a3bf88cb759c5",
+    "url": "/static/media/hero-compare-background.90807fe2.jpg"
+  },
+  {
+    "revision": "a98b61dd184421b611649e84077a9d35",
+    "url": "/static/media/hero-directory-background.a98b61dd.png"
+  },
+  {
+    "revision": "7432c0b891c1a46ba6b2463d5c927b1d",
+    "url": "/static/media/hero-directory-carousel.7432c0b8.jpg"
+  },
+  {
+    "revision": "1e5989b579c497bc96aff9c24ec6c42f",
+    "url": "/static/media/hero-show-background.1e5989b5.jpg"
+  },
+  {
+    "revision": "21eb222fb71b356856b5d69ed236fdc4",
+    "url": "/static/media/marketplace-carousel.21eb222f.jpg"
+  },
+  {
+    "revision": "529c3389a7b0bd399a4e211d789806bf",
+    "url": "/static/media/profile-page-background.529c3389.jpg"
+  },
+  {
+    "revision": "c2bcae4789e8619f5d054415945d7170",
+    "url": "/static/media/title-page-background.c2bcae47.jpg"
+  },
+  {
+    "revision": "f695c6e59f9f549a80f380d2df21709d",
+    "url": "/static/media/title-page-background.f695c6e5.gif"
+  },
+  {
+    "revision": "e6fee681ad8a32ba7016849ff836dfa8",
+    "url": "/static/media/welcome-carousel.e6fee681.jpg"
+  }
+]);
